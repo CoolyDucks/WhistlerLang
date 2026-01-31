ec4f433004f353e142754329c579f60c511cb91b
CoolyDucks <peguinboy2015@gmail.com>
2026-01-31 13:09:59 +0300

    Refactor math functions and add new operations
    Refactored math functions to accept float64 parameters instead of strings and added additional mathematical functions.
    (I can't do math system big..... Coming soon the math update)

--- source/math.go
+++ source/math.go
@@ -1,41 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"strconv"
-)
+import "math"
 
-func Add(a, b string) float64 {
-	x, _ := strconv.ParseFloat(a, 64)
-	y, _ := strconv.ParseFloat(b, 64)
-	return x + y
-}
-
-func Sub(a, b string) float64 {
-	x, _ := strconv.ParseFloat(a, 64)
-	y, _ := strconv.ParseFloat(b, 64)
-	return x - y
-}
-
-func Mul(a, b string) float64 {
-	x, _ := strconv.ParseFloat(a, 64)
-	y, _ := strconv.ParseFloat(b, 64)
-	return x * y
-}
-
-func Div(a, b string) float64 {
-	x, _ := strconv.ParseFloat(a, 64)
-	y, _ := strconv.ParseFloat(b, 64)
-	if y == 0 {
-		fmt.Println("Division by zero")
-		return 0
-	}
-	return x / y
-}
-
-func Pow(a, b string) float64 {
-	x, _ := strconv.ParseFloat(a, 64)
-	y, _ := strconv.ParseFloat(b, 64)
-	return math.Pow(x, y)
-}
+func Add(a,b float64) float64{return a+b}
+func Sub(a,b float64) float64{return a-b}
+func Mul(a,b float64) float64{return a*b}
+func Div(a,b float64) float64{return a/b}
+func Pow(a,b float64) float64{return math.Pow(a,b)}
+func Sqrt(a float64) float64{return math.Sqrt(a)}
+func Sin(a float64) float64{return math.Sin(a)}
+func Cos(a float64) float64{return math.Cos(a)}
+func Tan(a float64) float64{return math.Tan(a)}
+func Log(a float64) float64{return math.Log(a)}
+func Exp(a float64) float64{return math.Exp(a)}
+func Abs(a float64) float64{return math.Abs(a)}
+func Round(a float64) float64{return math.Round(a)}
