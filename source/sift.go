501534bda50579baf595d521ff6c742a43a08f24
CoolyDucks <peguinboy2015@gmail.com>
2026-01-31 13:08:52 +0300

    Refactor word splitting functionality in sift.go
    Refactor SplitWords to Sift for regex-based word extraction and add runtimeSift for printing results.

--- source/sift.go
+++ source/sift.go
@@ -1,31 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 )
 
-func SplitWords(line string) []string {
-	return strings.Fields(line)
+func Sift(line string)[]string{
+	line=strings.TrimSpace(line)
+	re:=regexp.MustCompile(`[^\s]+`)
+	return re.FindAllString(line,-1)
 }
 
-func Contains(slice []string, word string) bool {
-	for _, w := range slice {
-		if w == word {
-			return true
-		}
-	}
-	return false
-}
-
-func Join(words []string, sep string) string {
-	return strings.Join(words, sep)
-}
-
-func Index(slice []string, word string) int {
-	for i, w := range slice {
-		if w == word {
-			return i
-		}
-	}
-	return -1
+func runtimeSift(line string){
+	parts:=Sift(line)
+	fmt.Println("Sift result:",parts)
 }
